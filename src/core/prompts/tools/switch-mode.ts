98b6c349ad3bc8da9144575de857b2f2cb66fb95
vadash <vadash@gmail.com>
2025-02-28 16:36:18 +0300

    Trim switch-mode

--- src/core/prompts/tools/switch-mode.ts
+++ src/core/prompts/tools/switch-mode.ts
@@ -1,18 +1,14 @@
 export function getSwitchModeDescription(): string {
-	return `## switch_mode
-Description: Request to switch to a different mode. This tool allows modes to request switching to another mode when needed, such as switching to Code mode to make code changes. The user must approve the mode switch.
+	return `
+## switch_mode
+Description: Request to switch to different mode. Allows modes to request switching to another mode when needed, such as switching to Code mode for code changes. User must approve mode switch.
 Parameters:
-- mode_slug: (required) The slug of the mode to switch to (e.g., "code", "ask", "architect")
-- reason: (optional) The reason for switching modes
+- mode_slug: (required) Slug of mode to switch to (e.g., "code", "ask", "architect")
+- reason: (optional) Reason for switching modes
 Usage:
 <switch_mode>
 <mode_slug>Mode slug here</mode_slug>
 <reason>Reason for switching here</reason>
 </switch_mode>
-
-Example: Requesting to switch to code mode
-<switch_mode>
-<mode_slug>code</mode_slug>
-<reason>Need to make code changes</reason>
-</switch_mode>`
+`
 }
