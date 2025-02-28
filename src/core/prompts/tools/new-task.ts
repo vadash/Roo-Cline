38b9530dc09bd2af0320b9cdd598994e65a9bc66
vadash <vadash@gmail.com>
2025-02-28 16:37:30 +0300

    Trim new-task

--- src/core/prompts/tools/new-task.ts
+++ src/core/prompts/tools/new-task.ts
@@ -1,23 +1,16 @@
 import { ToolArgs } from "./types"
 
 export function getNewTaskDescription(args: ToolArgs): string {
-	return `## new_task
-Description: Create a new task with a specified starting mode and initial message. This tool instructs the system to create a new Cline instance in the given mode with the provided message.
-
+	return `
+## new_task
+Description: Create new task with specified starting mode and initial message. Instructs system to create new Cline instance in given mode with provided message.
 Parameters:
-- mode: (required) The slug of the mode to start the new task in (e.g., "code", "ask", "architect").
-- message: (required) The initial user message or instructions for this new task.
-
+- mode: (required) Slug of mode to start new task in (e.g., "code", "ask", "architect")
+- message: (required) Initial user message or instructions for new task
 Usage:
 <new_task>
 <mode>your-mode-slug-here</mode>
 <message>Your initial instructions here</message>
-</new_task>
-
-Example:
-<new_task>
-<mode>code</mode>
-<message>Implement a new feature for the application.</message>
 </new_task>
 `
 }
